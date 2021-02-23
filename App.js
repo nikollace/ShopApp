9faec41405a08de9f7450725f34fe26cb04f84e0
nikollace <nikoladjordjevicfon@gmail.com>
2021-02-23 13:05:59 +0100

    Odradjen pocetak ProductOverviewScreen-a, kao i ShopNavigator

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productReducer from './store/reducers/products'
+import ShopNavigator from './navigation/ShopNavigator'
+
+const rootReducer = combineReducers({
+  products: productReducer
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View >
-      <Text>Welcome to new app!</Text>
-    </View>
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
   );
 }
